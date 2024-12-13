94edee38b4255c801da056bc06d786345c58283d
김건학 <kkh10002@naver.com>
2024-12-13 18:49:55 +0900

    feat(TimeTable): 순환하는 기능 구현
    자료구조를 Queue로 두어, 근무를 하고나면 맨 뒤로 옮기도록 했습니다.

--- java-oncall-6/src/main/java/oncall/domain/TimeTable.java
+++ java-oncall-6/src/main/java/oncall/domain/TimeTable.java
@@ -2,19 +2,36 @@ package oncall.domain;
 
 import static oncall.constants.Messages.INVALID_INPUT_VALUE;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class TimeTable {
-    private final List<String> weekDays;
-    private final List<String> holidays;
+    private final Queue<String> weekDays;
+    private final Queue<String> holidays;
 
-    private TimeTable(final List<String> weekDays, final List<String> holidays) {
+    private TimeTable(final Queue<String> weekDays, final Queue<String> holidays) {
         this.weekDays = weekDays;
         this.holidays = holidays;
     }
+
     public static TimeTable of(final List<String> weekDays, final List<String> holidays) {
         checkSize(weekDays, holidays);
-        return new TimeTable(weekDays, holidays);
+        Queue<String> inputWeekDays = new LinkedList<>(weekDays);
+        Queue<String> inputHolidays = new LinkedList<>(holidays);
+        return new TimeTable(inputWeekDays, inputHolidays);
+    }
+    
+    public String getWeekDaysNickName() {
+        String nickName = weekDays.poll();
+        weekDays.add(nickName);
+        return nickName;
+    }
+
+    public String getHolidaysNickName() {
+        String nickName = holidays.poll();
+        holidays.add(nickName);
+        return nickName;
     }
 
     private static void checkSize(final List<String> weekDays, final List<String> holidays) {
